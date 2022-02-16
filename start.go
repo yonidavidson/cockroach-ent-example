94172b3e16f9f5d11f90a7ccea58c6a35f06f5a3
Yoni Davidson <yoni@ariga.io>
2022-02-16 15:47:17 +0200

    start: add running code

---
+++ start.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	dialect "entgo.io/ent/dialect"
+	_ "github.com/jackc/pgx/v4/stdlib"
+
+	entsql "entgo.io/ent/dialect/sql"
+
+	"github.com/yonidavidson/cockroachent/ent"
+	"github.com/yonidavidson/cockroachent/ent/account"
+)
+
+func main() {
+	databaseUrl := os.Getenv("COCKROACH_DSN")
+	db, err := sql.Open("pgx", databaseUrl)
+	if err != nil {
+		log.Fatal(err)
+	}
+	drv := entsql.OpenDB(dialect.Postgres, db)
+	client := ent.NewClient(ent.Driver(drv))
+	defer client.Close()
+	ctx := context.Background()
+	// run the auto migration tool.
+	if err := client.Schema.Create(ctx); err != nil {
+		log.Fatalf("failed creating schema resources: %v", err)
+	}
+	if _, err = CreateAccount(ctx, client); err != nil {
+		log.Fatal(err)
+	}
+	if _, err = QueryAccount(ctx, client); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func CreateAccount(ctx context.Context, client *ent.Client) (*ent.Account, error) {
+	u, err := client.Account.
+		Create().
+		SetBalance(35).
+		Save(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed creating account: %w", err)
+	}
+	log.Println("account was created: ", u)
+	return u, nil
+}
+
+func QueryAccount(ctx context.Context, client *ent.Client) (*ent.Account, error) {
+	u, err := client.Account.
+		Query().
+		Where(account.BalanceGT(20)).
+		First(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed querying account: %w", err)
+	}
+	log.Println("account returned: ", u)
+	return u, nil
+}
