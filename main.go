4f801beab2e5817437f62247a89377b2ee54bdbd
Yoni Davidson <yoni@ariga.io>
2022-02-17 15:48:43 +0200

    main.go: add transaction example (#6)

--- main.go
+++ main.go
@@ -32,30 +32,69 @@ func main() {
 	if err := client.Schema.Create(ctx); err != nil {
 		log.Fatalf("failed creating schema resources: %v", err)
 	}
-	if _, err = CreateAccount(ctx, client); err != nil {
+	// delete old accounts
+	if _, err := client.Account.Delete().Exec(ctx); err != nil {
 		log.Fatal(err)
 	}
-	a, err := QueryAccount(ctx, client)
+	// delete old users
+	if _, err := client.User.Delete().Exec(ctx); err != nil {
+		log.Fatal(err)
+	}
+	acc1, err := CreateAccount(ctx, client, 50)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if _, err = CreateUser(ctx, client); err != nil {
-		log.Fatal(err)
-	}
-	u, err := QueryUser(ctx, client)
+	acc2, err := CreateAccount(ctx, client, 50)
 	if err != nil {
 		log.Fatal(err)
 	}
-	if err := client.Account.UpdateOne(a).SetOwner(u).Exec(ctx); err != nil {
+	user1, err := CreateUser(ctx, client, "Yoni")
+	if err != nil {
 		log.Fatal(err)
 	}
-	log.Println("account connected to owner")
+	user2, err := CreateUser(ctx, client, "Amit")
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := client.Account.UpdateOne(acc1).SetOwner(user1).Exec(ctx); err != nil {
+		log.Fatal(err)
+	}
+	if err := client.Account.UpdateOne(acc2).SetOwner(user2).Exec(ctx); err != nil {
+		log.Fatal(err)
+	}
+
+	// get a transactional client
+	tx, err := client.Tx(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := tx.Account.UpdateOne(acc1).AddBalance(10).Exec(ctx); err != nil {
+		log.Fatal(err)
+	}
+	if err := tx.Account.UpdateOne(acc2).AddBalance(-10).Exec(ctx); err != nil {
+		log.Fatal(err)
+	}
+	if err := tx.Commit(); err != nil {
+		log.Fatal(err)
+	}
+	// end of transaction
+	acc1, err = QueryAccount(ctx, client, "Yoni")
+	if err != nil {
+		log.Fatal(err)
+	}
+	acc2, err = QueryAccount(ctx, client, "Amit")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Yoni's Account:", acc1)
+	fmt.Println("Amit's Account:", acc2)
+
 }
 
-func CreateAccount(ctx context.Context, client *ent.Client) (*ent.Account, error) {
+func CreateAccount(ctx context.Context, client *ent.Client, amount int) (*ent.Account, error) {
 	u, err := client.Account.
 		Create().
-		SetBalance(35).
+		SetBalance(amount).
 		Save(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("failed creating account: %w", err)
@@ -64,10 +103,10 @@ func CreateAccount(ctx context.Context, client *ent.Client) (*ent.Account, error
 	return u, nil
 }
 
-func QueryAccount(ctx context.Context, client *ent.Client) (*ent.Account, error) {
+func QueryAccount(ctx context.Context, client *ent.Client, owner string) (*ent.Account, error) {
 	u, err := client.Account.
 		Query().
-		Where(account.BalanceGT(20)).
+		Where(account.HasOwnerWith(user.NameEQ(owner))).
 		First(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("failed querying account: %w", err)
@@ -76,26 +115,14 @@ func QueryAccount(ctx context.Context, client *ent.Client) (*ent.Account, error)
 	return u, nil
 }
 
-func CreateUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
+func CreateUser(ctx context.Context, client *ent.Client, name string) (*ent.User, error) {
 	u, err := client.User.
 		Create().
-		SetName("Yoni").
+		SetName(name).
 		Save(ctx)
 	if err != nil {
 		return nil, fmt.Errorf("failed creating user: %w", err)
 	}
 	log.Println("user was created: ", u)
-	return u, nil
-}
-
-func QueryUser(ctx context.Context, client *ent.Client) (*ent.User, error) {
-	u, err := client.User.
-		Query().
-		Where(user.NameEQ("Yoni")).
-		First(ctx)
-	if err != nil {
-		return nil, fmt.Errorf("failed querying user: %w", err)
-	}
-	log.Println("user returned: ", u)
 	return u, nil
 }
